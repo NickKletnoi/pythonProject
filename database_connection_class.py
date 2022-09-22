2ea0b3a53ed7f7ac38ed047456cc9a17613f14f1
nick.kletnoi <nick.kletnoi@gmail.com>
2022-09-21 20:10:45 -0400

    all commits

---
+++ database_connection_class.py
@@ -0,0 +1,20 @@
+from config import Config
+import MySQLdb
+
+class Connection:
+    def __init__(self):
+        self.db=MySQLdb.connect(
+            Config.DATABASE_CONFIG['server'],
+            Config.DATABASE_CONFIG['user'],
+            Config.DATABASE_CONFIG['password'],
+            Config.DATABASE_CONFIG['name']
+            )
+        self.db.autocommit(True)
+        self.db.set_character_set('utf8mb4')
+        self.cur=self.db.cursor()
+
+
+
+
+sqlI = Connection()
+print(sqlI.query("SELECT `current_points` FROM `users` WHERE `nick` = 'username';"))
