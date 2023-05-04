b11a488d7df4b82692c52887d2cdabb10a816cbd
nick.kletnoi <nick.kletnoi@gmail.com>
2023-05-04 11:47:31 -0400

    change pointer to point to main prod table

--- RetrieveWaterLevels3d1/__init__.py
+++ RetrieveWaterLevels3d1/__init__.py
@@ -7,24 +7,25 @@ import datetime
 import pytz
 est_now = datetime.datetime.now().astimezone(pytz.timezone('US/Eastern'))
 now = datetime.datetime.now()
-next_day = est_now + datetime.timedelta(days = 1)
-previous_day = est_now + datetime.timedelta(days = -1)
+next_day = est_now + datetime.timedelta(days=1)
+previous_day = est_now + datetime.timedelta(days=-1)
 next_time = next_day.strftime("%Y-%m-%d %H:%M")
 current_time = est_now.strftime("%Y-%m-%d %H:%M")
 next_day_str = next_day.strftime("%Y%m%d")
 previous_day_str = previous_day.strftime("%Y%m%d")
-#print("tomorrow is: " + next_day_str)
-#print("yesterday was: " + previous_day_str)
+# print("tomorrow is: " + next_day_str)
+# print("yesterday was: " + previous_day_str)
 
 start_time = datetime.datetime.now()
 
-con_str = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=interlake-bi.database.windows.net;DATABASE=ISS_DW;UID=BIAdmin;PWD=sb98D&B(*#$@")
+con_str = urllib.parse.quote_plus(
+    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=interlake-bi.database.windows.net;DATABASE=ISS_DW;UID=BIAdmin;PWD=sb98D&B(*#$@")
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % con_str)
 
 
-stations_lst=[9063090, 9044020, 9044030, 9044036, 9044049, 9034052, 9014070, 9014090, 9014098, 9075099, 9076024,
+stations_lst = [9063090, 9044020, 9044030, 9044036, 9044049, 9034052, 9014070, 9014090, 9014098, 9075099, 9076024,
                 9076027, 9076033, 9076060, 9076070, 9099004, 9075080, 9087031, 9087044, 9087072, 9087057, 9099064,
-                9099018, 9063079, 9063053, 9063085, 9063063,9014080,9063038,9099090,9075035,9075065,9087077,9087096,9087023]
+                9099018, 9063079, 9063053, 9063085, 9063063, 9014080, 9063038, 9099090, 9075035, 9075065, 9087077, 9087096, 9087023]
 
 times_df = pd.DataFrame({"Time": [
     pd.Timestamp("01:00:00.000"),
@@ -53,12 +54,16 @@ times_df = pd.DataFrame({"Time": [
     pd.Timestamp("00:00:00.000")
 ]})
 
-base_url_part1 = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=' + previous_day_str + '&end_date=' + next_day_str + '&station='
+base_url_part1 = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=' + \
+    previous_day_str + '&end_date=' + next_day_str + '&station='
 base_url_part2 = '&interval=h&product=water_level&datum=LWD&time_zone=lst_ldt&application=Interlake&units=english&format=json'
 
-#def main(req: func.HttpRequest) -> func.HttpResponse:
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+
+
 def main(mytimer: func.TimerRequest) -> None:
-    final_station_list = [base_url_part1 + str(i) + base_url_part2 for i in stations_lst]
+    final_station_list = [base_url_part1 +
+                          str(i) + base_url_part2 for i in stations_lst]
     final_df = pd.DataFrame()
     for url in final_station_list:
         response = requests.get(url=url)
@@ -70,7 +75,8 @@ def main(mytimer: func.TimerRequest) -> None:
             stationid = station
             time = d['t']
             waterlevel = d['v']
-            rows.append({"StationId": stationid, "Time": time, "WaterLevel": waterlevel})
+            rows.append({"StationId": stationid, "Time": time,
+                        "WaterLevel": waterlevel})
         df = pd.DataFrame(rows, columns=dfcols)
         final_df = pd.concat([final_df, df], axis=0)
 
@@ -85,7 +91,8 @@ def main(mytimer: func.TimerRequest) -> None:
     stations_final_df.rename(columns={'Time': 'StationsTime'}, inplace=True)
 
     final_df = final_df[final_df.WaterLevel != '']
-    final_df = final_df.astype({'WaterLevel': 'float64', 'Time': 'datetime64[ns]', 'StationId': 'int64'})
+    final_df = final_df.astype(
+        {'WaterLevel': 'float64', 'Time': 'datetime64[ns]', 'StationId': 'int64'})
     final_df['WaterLevel'] = final_df['WaterLevel'].apply(lambda x: (x * 12))
     final_df.rename(columns={'Time': 'WaterlevelsTime'}, inplace=True)
 
@@ -100,7 +107,8 @@ def main(mytimer: func.TimerRequest) -> None:
 
     df_ff.rename(columns={'WaterlevelsTime': 'Time'}, inplace=True)
     df_ff = df_ff.drop(['StationsTime'], axis=1)
-    df_ff.to_sql('WaterLevelMeasurements2', con=engine, index=False, if_exists='append', schema='dbo')
+    df_ff.to_sql('WaterLevelMeasurements', con=engine,
+                 index=False, if_exists='append', schema='dbo')
 
     end_time = datetime.datetime.now()
     execution_time = end_time - start_time
@@ -109,5 +117,3 @@ def main(mytimer: func.TimerRequest) -> None:
     print("Success")
 
     return func.HttpResponse(f"Successfully executed  in  {execution_time} seconds")
-
-
