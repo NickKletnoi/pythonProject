4a101f464c4a7252d3b543fcfe741851c6890de7
nick.kletnoi <nick.kletnoi@gmail.com>
2023-05-05 22:34:07 -0400

    googleMaps POC

---
+++ GoogleMaps_GeoCode.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import googlemaps
+gmaps_key = googlemaps.Client(key="AIzaSyBe-50GHt8aJC75shtyhsw7-Ca2BXIsAlM")
+df = pd.read_csv('data/zomato1.csv',encoding="ISO-8859-1")
+
+def geocode(add):
+    g = gmaps_key.geocode(add)
+    lat = g[0]["geometry"]["location"]["lat"]
+    lng = g[0]["geometry"]["location"]["lng"]
+    return (lat, lng)
+
+df['geocoded'] = df['Address'].apply(geocode)
+
+new_df = df[['Location_ID','Address','geocoded']]
+print(new_df.to_string())
