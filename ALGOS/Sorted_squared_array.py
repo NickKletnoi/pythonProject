d44a009ca85038bf836a6dba4b27b9d1f58c4f5c
nick.kletnoi <nick.kletnoi@gmail.com>
2023-05-29 19:58:40 -0400

    algos

---
+++ ALGOS/Sorted_squared_array.py
@@ -0,0 +1,5 @@
+
+#square the items in the array and return the result
+array = [1,2,3,5,6,8,9]
+result = sorted([x**2 for x in array])
+print(result)
--- ALGOS/Sum_2_numbers_given_target_total.py
+++ ALGOS/Sum_2_numbers_given_target_total.py
@@ -1,5 +1,6 @@
 ###############################################################################
 ### return 2 numbers from the list only  if they total a given target s number
+##############################################################################
 ar = [3, 5, -4, 8, 11, 1, -1, 6]
 s = 10
 
@@ -7,7 +8,6 @@ res1 = [t for x in set(ar) if (t:=(s - x)) in set(ar) and t is not x]
 
 def twonumberSum(array, targetSum):
   storage = set(num for num in array)
-
   for num in array:
       target = targetSum - num
       if target in storage and target is not num:
