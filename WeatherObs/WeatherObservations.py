d569da247c030727b3ec2763bc2eb1e0ea0d6442
nick.kletnoi <nick.kletnoi@gmail.com>
2023-09-26 11:48:13 -0400

    added all of the required felds

--- WeatherObs/WeatherObservations.py
+++ WeatherObs/WeatherObservations.py
@@ -12,13 +12,16 @@ engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % con_str)
 
 #stations_lst=[9063090, 9044020, 9044030, 9044036, 9044049, 9034052, 9014070, 9014090, 9014098, 9075099, 9076024,9076027, 9076033, 9076060, 9076070, 9099004, 9075080, 9087031, 9087044, 9087072, 9087057, 9099064,9099018, 9063079, 9063053, 9063085,9063063,9014080, 9063038, 9099090,9075035, 9075065, 9087077, 9087096, 9087023]
 
-stations_lst=[9063063]
+stations_lst=[9063063,9075099,9075099,9076027,9076033,9076070,9075080,9087031,9099064,9063079,9063053,9063063,9075065]
 
 base_url_part1 = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&station='
-base_url_part2 = '&product=wind&datum=IGLD&time_zone=gmt&units=english&format=json'
+base_url_part2_wind = '&product=wind&datum=IGLD&time_zone=gmt&units=english&format=json'
+base_url_part2_airtemp = '&product=air_temperature&datum=IGLD&time_zone=gmt&units=english&format=json'
+base_url_part2_airpress = '&product=air_pressure&datum=IGLD&time_zone=gmt&units=english&format=json'
+base_url_part2_watertemp = '&product=water_temperature&datum=IGLD&time_zone=gmt&units=english&format=json'
 
-def ingest_from_noaa():
-    final_station_list = [base_url_part1 + str(i) + base_url_part2 for i in stations_lst]
+def ingest_wind_from_noaa():
+    final_station_list = [base_url_part1 + str(i) + base_url_part2_wind for i in stations_lst]
     final_df = pd.DataFrame()
     for url in final_station_list:
         response = requests.get(url=url)
@@ -36,7 +39,7 @@ def ingest_from_noaa():
         final_df = pd.concat([final_df, df], axis=0)
 
     final_df = final_df.astype({'StationId': 'int64','ObservationTime': 'datetime64[ns]','WindSpeed': 'float64','WindDirection': 'string'})
-    final_df.to_sql('WeatherObservations_stg', con=engine, index=False, if_exists='append', schema='dbo')
+    final_df.to_sql('WeatherObservations_Wind_stg', con=engine, index=False, if_exists='append', schema='dbo')
 
     end_time = datetime.datetime.now()
     execution_time = end_time - start_time
@@ -45,4 +48,97 @@ def ingest_from_noaa():
     print("Success")
 
 
-ingest_from_noaa()
+def ingest_airtemp_from_noaa():
+    final_station_list = [base_url_part1 + str(i) + base_url_part2_airtemp for i in stations_lst]
+    final_df = pd.DataFrame()
+    for url in final_station_list:
+        response = requests.get(url=url)
+        data = response.json()
+        dfcols = ["StationId", "ObservationTime", "AirTemperature"]
+        station = data['metadata']['id']
+        rows = []
+        for d in data['data']:
+            stationid = station
+            time = d['t']
+            airtemp = d['v']
+            rows.append({"StationId": stationid, "ObservationTime": time, "AirTemperature": airtemp})
+        df = pd.DataFrame(rows, columns=dfcols)
+        final_df = pd.concat([final_df, df], axis=0)
+
+    final_df = final_df.astype({'StationId': 'int64','ObservationTime': 'datetime64[ns]','AirTemperature': 'float64'})
+    final_df.to_sql('WeatherObservations_AirTemp_stg', con=engine, index=False, if_exists='append', schema='dbo')
+
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"execution time was: {execution_time}")
+    print(final_df.to_string())
+    print("Success")
+
+
+def ingest_watertemp_from_noaa():
+    final_station_list = [base_url_part1 + str(i) + base_url_part2_watertemp for i in stations_lst]
+    final_df = pd.DataFrame()
+    for url in final_station_list:
+        response = requests.get(url=url)
+        data = response.json()
+        dfcols = ["StationId", "ObservationTime", "WaterTemperature"]
+        station = data['metadata']['id']
+        rows = []
+        for d in data['data']:
+            stationid = station
+            time = d['t']
+            airtemp = d['v']
+            rows.append({"StationId": stationid, "ObservationTime": time, "WaterTemperature": airtemp})
+        df = pd.DataFrame(rows, columns=dfcols)
+        final_df = pd.concat([final_df, df], axis=0)
+
+    final_df = final_df.astype({'StationId': 'int64','ObservationTime': 'datetime64[ns]','WaterTemperature': 'float64'})
+    final_df.to_sql('WeatherObservations_WaterTemp_stg', con=engine, index=False, if_exists='append', schema='dbo')
+
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"execution time was: {execution_time}")
+    print(final_df.to_string())
+    print("Success")
+
+def ingest_airpress_from_noaa():
+    final_station_list = [base_url_part1 + str(i) + base_url_part2_airpress for i in stations_lst]
+    final_df = pd.DataFrame()
+    for url in final_station_list:
+        response = requests.get(url=url)
+        data = response.json()
+        dfcols = ["StationId", "ObservationTime", "AirPressure"]
+        station = data['metadata']['id']
+        rows = []
+        for d in data['data']:
+            stationid = station
+            time = d['t']
+            airpress = d['v']
+            rows.append({"StationId": stationid, "ObservationTime": time, "AirPressure": airpress})
+        df = pd.DataFrame(rows, columns=dfcols)
+        final_df = pd.concat([final_df, df], axis=0)
+
+    final_df = final_df.astype({'StationId': 'int64','ObservationTime': 'datetime64[ns]','AirPressure': 'float64'})
+    final_df.to_sql('WeatherObservations_AirPressure_stg', con=engine, index=False, if_exists='append', schema='dbo')
+
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"execution time was: {execution_time}")
+    print(final_df.to_string())
+    print("Success")
+
+def final_table_assemble():
+        dfcols = ["InsertTime", "StoredProc"]
+        procname = "sp_weatherobs_insert"
+        rows = []
+        rows.append({"InsertTime": start_time, "StoredProc": procname})
+        df = pd.DataFrame(rows, columns=dfcols)
+
+        df = df.astype({'InsertTime': 'datetime64[ns]', 'StoredProc': 'string'})
+        df.to_sql('WeatherObservations_Log', con=engine, index=False, if_exists='append', schema='dbo')
+
+ingest_wind_from_noaa()
+ingest_airtemp_from_noaa()
+ingest_watertemp_from_noaa()
+ingest_airpress_from_noaa()
+final_table_assemble()
