e43e82fde333bfc01e62647595253ff0cde97c4c
nick.kletnoi <nick.kletnoi@gmail.com>
2023-10-05 09:48:33 -0400

    langchain enter

---
+++ langchain/langchain2.py
@@ -0,0 +1,32 @@
+import os
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from langchain.llms import OpenAI
+import pandas as pd
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
+
+OpenAI_key = os.environ.get("OPENAI_API_KEY")
+df = pd.read_csv("C:\\Users\\kletn\\PycharmProjects\\PythonProject\\data\\titanic.csv")
+
+#print(OpenAI_key)
+
+#openai_api_key="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"
+agent = create_pandas_dataframe_agent(
+    ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613",openai_api_key=OpenAI_key),
+    df,
+    verbose=True,
+    agent_type=AgentType.OPENAI_FUNCTIONS,
+)
+
+df1 = df.copy()
+df1["Age"] = df1["Age"].fillna(df1["Age"].mean())
+
+agent = create_pandas_dataframe_agent(OpenAI(temperature=0), [df, df1], verbose=True)
+result = agent.run("how many rows in the age column are different?")
+
+
+#pip install --upgrade pydantic
+#pip install --upgrade langchain
+
--- spacy/new_spacy_test.py
+++ spacy/new_spacy_test.py
@@ -3,5 +3,75 @@ import spacy
 nlp = spacy.load('en_core_web_sm')
 doc = nlp(u'Tesla is looking at buying U.S. startup for $6 million')
 
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+mystring = '"We\'re moving to L.A.!"'
+
+# for token in doc:
+#     print(token.text, token.pos_, token.dep_)
+
+#nlp.pipeline
+#tagger, parser, ner
+
+doc = nlp(mystring)
+
+# for token in doc:
+#     #print(token, token.pos_, token.dep_)
+#     print(token)
+
+#print(doc[3:6])
+
+doc8 = nlp(u"Apple to build a Hong Kong factory for $6 million")
+
+# for token in doc8:
+#     print(token.text, end=' | ')
+
+
+# for entity in doc8.ents:
+#     print(entity)
+#     print(entity.label_)
+#     print(str(spacy.explain(entity.label_)))
+#     print('\n')
+
+
+doc9 = nlp(u"Autonomous cars shift insurance liability toward manufacturers.")
+
+for chunk in doc9.noun_chunks:
+    print(chunk.text)
+
+
+from spacy import displacy
+
+doc = nlp(u"Apple is going to build a U.K. factory for $6 million.")
+
+#displacy.render(doc, style='dep', jupyter=False, options={'distance': 110})
+#displacy.serve(doc, style='dep')
+
+### stemming #######
+
+
+from nltk.stem.porter import PorterStemmer
+
+p_stemmer = PorterStemmer()
+words = ['run', 'runner', 'ran', 'runs', 'easily', 'fairly']
+
+# for word in words:
+#     print(word + '------->' + p_stemmer.stem(word))
+
+from nltk.stem.snowball import SnowballStemmer
+
+s_stemmer = SnowballStemmer(language='english')
+
+words = ['run', 'runner', 'ran', 'runs', 'easily', 'fairly', 'fairness','generous', 'generation', 'generate']
+
+# for word in words:
+#     print(word + '------->' + s_stemmer.stem(word))
+
+
+##### Lemmitization #####
+
+# for word in words:
+#     print(word + '------->' + s_stemmer.stem(word))
+
+doc10 = nlp(u"I am a runner running in a race because I love to run since I ran today")
+
+for token in doc10:
+    print(f'{token.text:{10}} {token.pos_:{8}} {token.lemma:<{22}} {token.lemma_}')
